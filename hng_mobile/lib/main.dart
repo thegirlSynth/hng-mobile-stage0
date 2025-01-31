49114350a760366354a10163adbc3360f4eefa11
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2025-01-31 01:20:39 +0100

    [FIX]: Update UI

--- hng_mobile/lib/main.dart
+++ hng_mobile/lib/main.dart
@@ -34,12 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -47,16 +41,33 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+             Text(
+              'Welcome to HNG stage 0',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.purple[800]),
             ),
-            ElevatedButton(
-              onPressed: () {
-                launchUrl(Uri.parse("https://github.com/thegirlSynth/hng-mobile-stage0"));
-              },
-              child: const Text("View Github Repo"),
+
+            const Text(
+              'This project is hosted on Github.',
+            ),
+
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    launchUrl(Uri.parse("https://github.com/thegirlSynth/hng-mobile-stage0"));
+                  },
+                  child: const Text("View Github Repo"),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    launchUrl(Uri.parse("http://hng.tech/hire/flutter-developers"));
+                  },
+                  child: const Text("Hire a Flutter Developer"),
+                ),
+              ],
             ),
           ],
         ),
